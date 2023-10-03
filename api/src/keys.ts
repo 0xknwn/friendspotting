10a9c05b15b8dd118c4cc000105bdc297ade686c
0xknwn <145777008+0xknwn@users.noreply.github.com>
2023-10-03 22:46:56 +0200

    remove dependency to friendmex

--- api/src/keys.ts
+++ api/src/keys.ts
@@ -9,6 +9,7 @@ export const keyHistory = (prisma: PrismaClient) => {
       return;
     }
     const history = await prisma.trade.findMany({
+      select: { timestamp: true, supply: true },
       where: { subjectAddress: key },
       orderBy: [{ timestamp: "desc" }],
     });
