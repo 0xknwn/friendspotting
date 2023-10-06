741bffa09e9291f4eb811172b9bc2a5f16193de2
0xknwn <145777008+0xknwn@users.noreply.github.com>
2023-10-06 14:08:43 +0200

    better logs

--- api/src/friendtech.ts
+++ api/src/friendtech.ts
@@ -1,31 +1,18 @@
 import { friendtechABI } from "./abi/friendtech";
 import { getContract, Address } from "viem";
-import { readFileSync } from "fs";
+import { getMnemonic } from "./mnemonic";
 
 import { goerliPublicClient, goerliWalletClient } from "./wallet";
 
 export const friendtechAddress =
   "0xa7c1de4d98d2564f3b3b97218b398db51c195f4d" as Address;
 
-export const getMnemonic = () => {
-  if (process.env.MNEMONIC_FILE) {
-    const mnemonic = readFileSync(process.env.MNEMONIC_FILE, {
-      encoding: "utf8",
-    });
-    if (mnemonic.length > 0) {
-      return mnemonic;
-    }
-  }
-  throw "mnemonic file not found";
-};
-
-export const setSharesSupply = async (
-  address: `0x${string}`,
-  value: bigint
-) => {
+export const setSharesSupply = async (address: Address, value: bigint) => {
   const goerli = await goerliPublicClient();
   const mnemonic = getMnemonic();
   const wallet = await goerliWalletClient(mnemonic);
+  console.log("configuration file", process.env.MNEMONIC_FILE);
+  console.log("writing from", wallet.account.address);
   const friendtechContract = getContract({
     address: friendtechAddress,
     abi: friendtechABI,
