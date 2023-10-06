9da3b6b448652327c6bd8c20958d232ccb5c5e2a
0xknwn <145777008+0xknwn@users.noreply.github.com>
2023-10-06 15:15:50 +0200

    add a test to control the address

--- api/src/integration/mnemonic.test.ts
+++ api/src/integration/mnemonic.test.ts
@@ -1,13 +1,18 @@
 import { expect, test } from "vitest";
 import { generateMnemonic } from "bip39";
 import { getMnemonic } from "../mnemonic";
+import { goerliWalletClient } from "../wallet";
 
 test.skip("check the Mnemonic are 12 words", () => {
   const mnemonic = generateMnemonic();
   expect(mnemonic.split(" ").length).toBe(12);
 });
 
-test("check the Mnemonic file exists", () => {
+test("check the container key matches the mnemonic", async () => {
   const mnemonic = getMnemonic();
+  const wallet = await goerliWalletClient(mnemonic);
   expect(mnemonic.split(" ").length).toBe(12);
+  expect(wallet.account.address).toBe(
+    `0xE0d4ec43778010675264c6298eE221f8986DBb25`
+  );
 });
