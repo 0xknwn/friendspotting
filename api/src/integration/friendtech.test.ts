2d16ecb083b5cbac726c5a73a655b1bf31446986
0xknwn <145777008+0xknwn@users.noreply.github.com>
2023-10-06 14:30:20 +0200

    update test with key

--- api/src/integration/friendtech.test.ts
+++ api/src/integration/friendtech.test.ts
@@ -2,23 +2,18 @@ import { expect, test } from "vitest";
 import { getSharesSupply, setSharesSupply } from "../friendtech";
 import { wait } from "../util";
 
+const destAddress = `0xef42b587e4a3d33f88fa499be1d80c676ff7a226`;
+
 test.skip(
   "set a shared supply with the system",
   async () => {
-    const currentSupply = await getSharesSupply(
-      `0x26BE4e415B38738A5E1845A3dc0cd4Eb26718815`
-    );
+    const currentSupply = await getSharesSupply(destAddress);
     console.log("current supply", currentSupply);
-    const receipt = await setSharesSupply(
-      `0x26BE4e415B38738A5E1845A3dc0cd4Eb26718815`,
-      currentSupply + 1n
-    );
+    const receipt = await setSharesSupply(destAddress, currentSupply + 1n);
     console.log(receipt);
     expect(receipt.status).toBe("success");
     await wait(5000);
-    const newSupply = await getSharesSupply(
-      `0x26BE4e415B38738A5E1845A3dc0cd4Eb26718815`
-    );
+    const newSupply = await getSharesSupply(destAddress);
     console.log("new supply", newSupply);
     expect(newSupply).toBe(currentSupply + 1n);
   },
