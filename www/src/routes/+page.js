7f9762d52c187643782f2e8ab8e45a47f44db995
0xknwn <145777008+0xknwn@users.noreply.github.com>
2023-10-12 20:26:44 +0200

    initialize svelte project

--- www/src/routes/+page.js
+++ www/src/routes/+page.js
@@ -1,3 +1,12 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
+
+/** @type {import('./$types').PageLoad} */
+export async function load({ params }) {
+	const out = await fetch('https://api.fren.bond/top50/today');
+	const top50 = await out.json();
+	return {
+		top50
+	};
+}
