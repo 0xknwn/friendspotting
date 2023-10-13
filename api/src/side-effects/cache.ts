36aa5a3e349a84d759b1d4c894fc688882820cac
0xknwn <145777008+0xknwn@users.noreply.github.com>
2023-10-13 09:58:23 +0200

    connect to redis

--- api/src/side-effects/cache.ts
+++ api/src/side-effects/cache.ts
@@ -1,20 +1,21 @@
 import { createClient } from "redis";
 
-const client = () => {
-  return createClient({
+const client = async () => {
+  const client = createClient({
     url: `redis://${process.env.REDIS_HOST || "redis"}:6379`,
   });
+  return await client.connect();
 };
 
 export const save = async (key: string, value: any) => {
-  const output = await client().set(key, JSON.stringify(value));
+  const output = await (await client()).set(key, JSON.stringify(value));
   if (output !== "OK") {
     throw "could not cache the key";
   }
 };
 
 export const retrieve = async (key: string) => {
-  const output = await client().get(key);
+  const output = await (await client()).get(key);
   if (!output) {
     throw "could not retrieve the key";
   }
